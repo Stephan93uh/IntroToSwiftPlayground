a764b8703283a85cd02a044fe3159999182b2617
Stephan93uh <stephan9@hawaii.edu>
2022-09-07 15:43:23 -1000

    Completed Assignment #2
    Intro to Bolean variables

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -16,3 +16,16 @@ var lastName = "Bradley"
 
 var greeting = opening + " " + firstName + " " + lastName
 print(greeting)
+
+// Assignment #2 Introduction to True or False
+var appState = false
+print(appState)
+
+var numberOne = 5
+var numberTwo = 7
+var comparison = numberOne < numberTwo
+
+var boardgameColor = "yellow"
+var apparelColor = "red"
+var sameFavColor = boardgameColor == apparelColor
+print(sameFavColor)
